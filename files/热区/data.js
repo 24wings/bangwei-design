a2b626cd41401b18281dd71699d2d48d15931cd8
24wings <2121718893@qq.com>
2018-04-14 09:47:22 +0800

    s

---
+++ files/热区/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),bk,_(bl,bm,bn,bm)),S,_(),bo,_())])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="热区.html",d="generationDate",e=new Date(1523670199474.9),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="acccfcd9f7044bc495f7de110662e99a",q="type",r="Axure:Page",s="name",t="热区",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="d6e9a08525244ec29196f08669a05d16",Y="label",Z="friendlyType",ba="Hot Spot",bb="imageMapRegion",bc="styleType",bd="visible",be=true,bf="location",bg="x",bh=20,bi="y",bj=10,bk="size",bl="width",bm=50,bn="height",bo="imageOverrides",bp="masters",bq="objectPaths",br="d6e9a08525244ec29196f08669a05d16",bs="scriptId",bt="u242";
+return _creator();
+})());
